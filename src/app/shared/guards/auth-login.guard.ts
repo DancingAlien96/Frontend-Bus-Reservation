abf6ce368299c613126225ffefa767d8df65a750
KenethUrrutia <kenethurrutia24d@gmail.com>
2024-10-22 12:02:30 -0600

    Identacion y eliminar logs

--- src/app/shared/guards/auth-login.guard.ts
+++ src/app/shared/guards/auth-login.guard.ts
@@ -2,13 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const authLoginGuard: CanActivateFn = (route, state) => {
+	const token = sessionStorage.getItem('token');
 
-  const token = sessionStorage.getItem('token');
-  
-  if(token){
-  const router = inject(Router);
-  router.navigateByUrl('solicitudes');
-  return true;
-  }
-  return false;
+	if (token) {
+		const router = inject(Router);
+		router.navigateByUrl('solicitudes');
+		return true;
+	}
+	return false;
 };
