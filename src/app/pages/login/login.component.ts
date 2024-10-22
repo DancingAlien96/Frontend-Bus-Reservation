d4a3e9bf037b56123d0ff06b3451c30a46ec7f73
KenethUrrutia <kenethurrutia24d@gmail.com>
2024-10-22 12:02:21 -0600

    agrega mensaje de error si las credenciasles de login son incorrectas

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -5,8 +5,9 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { CookieService } from 'ngx-cookie-service';
 import { UsuarioInterface } from '../../shared/interfaces/usuario.interface';
+import { catchError, of } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
 	selector: 'app-login',
@@ -19,7 +20,7 @@ export class LoginComponent {
 	public formSubmit: FormGroup;
 	public user!: UsuarioInterface;
 
-	constructor(private fb: FormBuilder, private loginService: LoginService, private cookies: CookieService) {
+	constructor(private fb: FormBuilder, private loginService: LoginService, private _snakBar: MatSnackBar) {
 		this.formSubmit = this.fb.group({
 			USERNAME: [null, Validators.required],
 			PASSWORD: [null, [Validators.required]]
@@ -28,16 +29,25 @@ export class LoginComponent {
 
 	onSubmit() {
 		if (this.formSubmit.valid) {
-			this.loginService.access(this.formSubmit.value).subscribe((res) => {
-				console.log(res);
-			   
-				sessionStorage.setItem('token', res.token);
-				sessionStorage.setItem('usuario', JSON.stringify(res.usuario));
-
-				
-			});
+			this.loginService
+				.access(this.formSubmit.value)
+				.pipe(
+					catchError((error) => {
+						if (error.status === 404) {
+							this._snakBar.open(error.error.message, 'Cerrar', { duration: 3000 });
+						} else {
+							this._snakBar.open('Error en el servidor', 'Cerrar', { duration: 3000 });
+						}
+						return of(null); // Retorna un observable vacío para continuar el flujo
+					})
+				)
+				.subscribe((res) => {
+					if (res) {
+						sessionStorage.setItem('token', res.token);
+						sessionStorage.setItem('usuario', JSON.stringify(res.usuario));
+					}
+				});
 		} else {
-			
 		}
 	}
 }
