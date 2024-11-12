4b318615e70b1f7e11345ff31a173506838dc5ad
cristoferperez <cristoferperez3@gmail.com>
2024-11-12 15:14:45 -0600

    se agrego en el front el recaptcha de google aun sin funcion en back

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { AuthService } from './shared/services/auth.service';
 import { LoginComponent } from './pages/login/login.component';
+import { ReCaptchaV3Service } from 'ng-recaptcha';
 
 @Component({
 	selector: 'app-root',
@@ -12,5 +13,12 @@ import { LoginComponent } from './pages/login/login.component';
 })
 export class AppComponent {
 	title = 'Sistema de gestion de solicitudes de vehiculos universitarios';
-	constructor(public authService: AuthService) {}
+	constructor(public authService: AuthService,
+   private recaptchaV3Service: ReCaptchaV3Service
+
+	) {}
+
+	public executeImportantAction(): void {
+    this.recaptchaV3Service.execute('importantAction')
+      .subscribe((token) =>console.log(token));}
 }
